bab206c9c1f80c0349b92942375202bafe07bc7a
Simon Opheim <simon.opheim@gmail.com>
2019-05-23 10:42:04 +0200

    async functions done

---
+++ main.js
@@ -0,0 +1,40 @@
+
+const stationsInfoUrl = "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json";
+const availabilityUrl = "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json";
+
+let stations;
+
+//get data
+async function getStationsData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.data.stations
+}
+
+//combine data
+async function updateAvailability(oldData) {
+    const updatedAvailabilityData = await getStationsData(availabilityUrl);
+    const newData = oldData.map(station => {
+        const stationAvailability = updatedAvailabilityData.find(stationAvailabilityData => {
+           return stationAvailabilityData.station_id == station.station_id
+        });
+        return {...station, ...stationAvailability}
+        //if two names in merging object are the same, the last object (stationAvailability) will overwrite the first (station)
+        //this means changed data will be updated! =)
+    })
+    return newData
+}
+
+//update data
+async function refresh() {
+    try {
+        const updatedData = await updateAvailability(stations);
+        stations = updatedData; //update global variable "stations" with new up-to-date data
+        updateTable(updatedData);
+        updateLastUpdatedText();
+    } catch(err) {
+        console.error("Error updating!", err)
+    }
+}
+
+//show data
